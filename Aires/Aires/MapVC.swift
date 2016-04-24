217393f41fe30bc9ac341666aac0ff91fa0a3191
sarayd <saradubusiness@gmail.com>
2016-04-23 17:31:31 -0700

    commit

--- Aires/Aires/MapVC.swift
+++ Aires/Aires/MapVC.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 import MapKit
+import Alamofire
 
 class MapVC: UIViewController, MKMapViewDelegate {
+    let url = "http://masanoriuehara.com/api/aircheck/";
+    var json = JSON([]);
+    
+    var longitude = [CLLocationDegrees]()
+    var latitude = [CLLocationDegrees]()
+    var location = [CLLocationCoordinate2D]()
     
     @IBOutlet weak var mapView: MKMapView!
     var annotations = [MKPointAnnotation]()
-    var type = "Cough"
+    var fakelocationarray = [CLLocationCoordinate2D]()
     override func viewDidLoad() {
+        
+        let params = [
+            "user_id" : 1,
+            "func" : "timeline"
+        ];
+        
+        Alamofire.request(.GET, url, parameters: params, encoding: ParameterEncoding.URL).responseJSON { (_, _, result) in
+            switch result {
+            case .Success(let data):
+                self.json = JSON(data)
+                print(self.json)
+                for index in 0...self.json.count-1{
+                    self.latitude.append(Double(self.json[index]["latitude"].rawString()!)!)
+                    self.longitude.append(Double(self.json[index]["longitude"].rawString()!)!)
+                    print(self.json[index]["longitude"])
+                    print(self.json[index]["latitude"])
+
+                }
+                for index in 0...self.latitude.count-1{
+                    print("Worked")
+                    self.location.append(CLLocationCoordinate2D(latitude: self.latitude[index], longitude: self.longitude[index]))
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = self.location[index]
+                    self.mapView.addAnnotation(annotation)
+                }
+                
+                self.mapView.reloadInputViews()
+            case .Failure(_, let error):
+                print("Request failed with error: \(error)")
+            }
+        }
         
         //*IMPORTANT*
         mapView.delegate = self
         
+        
         //Irvine
+        /*
         let location = CLLocationCoordinate2D(
             latitude: 33.6839,
             longitude:  -117.8222222
@@ -37,125 +77,53 @@ class MapVC: UIViewController, MKMapViewDelegate {
         annotation.subtitle = "8"
         annotations.append(annotation)
         mapView.addAnnotation(annotation)
+
+        let location1 = CLLocationCoordinate2D(
+            latitude: 33.7000,
+            longitude:  -117.8222222
+        
+        )
+        let annotation1 = MKPointAnnotation()
+        annotation1.coordinate = location1
+        annotation1.title = "Breath"
+        annotation1.subtitle = "8"
+        annotations.append(annotation1)
+        mapView.addAnnotation(annotation1)
+      */
         
     }
+    
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
         // simple and inefficient example
         
         let annotationView = MKPinAnnotationView()
-        if(type == "Cough"){
+        let x = annotation as! MKPointAnnotation
+        if(x.title == "Breath"){
+            annotationView.pinTintColor = UIColor.blueColor()
+        }
+        else if(x.title == "Cough"){
             annotationView.pinTintColor = UIColor.greenColor()
-        }else if(type == "Breath"){
+        }
+        else if(x.title == "Breath"){
             annotationView.pinTintColor = UIColor.purpleColor()
-        }else if(type == "Wheeze"){
+        }else if(x.title == "Wheeze"){
             annotationView.pinTintColor = UIColor.blueColor()
 
-        }else if(type == "Nasal"){
+        }else if(x.title == "Nasal"){
             annotationView.pinTintColor = UIColor.brownColor()
 
-        }else if(type == "Sneeze"){
+        }else if(x.title == "Sneeze"){
             annotationView.pinTintColor = UIColor.yellowColor()
-
         }else{
             annotationView.pinTintColor = UIColor.redColor()
 
         }
+        
+        mapView.selectAnnotation(annotation, animated: true)
+
         return annotationView
     }
-
-    @IBAction func coughPressed(sender: AnyObject) {
-        type = "Cough"
-        mapView.removeAnnotations(annotations)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-        )
-        annotation.title = "Cough"
-        annotation.subtitle = "8"
-        annotations.append(annotation)
-        mapView.addAnnotation(annotation)
-        mapView.setCenterCoordinate(CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-            )
-            , animated: true)
-    }
-  
-    @IBAction func breathPressed(sender: AnyObject) {
-        type = "Breath"
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-        )
-        annotation.title = "Cough"
-        annotation.subtitle = "8"
-        annotations.append(annotation)
-        mapView.removeAnnotations(annotations)
-        mapView.setCenterCoordinate(mapView.region.center, animated: true)
-
-    }
     
-    @IBAction func wheezePressed(sender: AnyObject) {
-        type = "Wheeze"
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-        )
-        annotation.title = "Cough"
-        annotation.subtitle = "8"
-        annotations.append(annotation)
-        mapView.removeAnnotations(annotations)
-
-    }
-   
-    @IBAction func nasalObstruction(sender: AnyObject) {
-        type = "Nasal"
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-        )
-        annotation.title = "Cough"
-        annotation.subtitle = "8"
-        annotations.append(annotation)
-        mapView.removeAnnotations(annotations)
-
-    }
-    @IBAction func sneezingPressed(sender: AnyObject) {
-        type = "Sneeze"
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-        )
-        annotation.title = "Cough"
-        annotation.subtitle = "8"
-        annotations.append(annotation)
-        mapView.removeAnnotations(annotations)
-
-    }
-    @IBAction func itchyEyes(sender: AnyObject) {
-        type = "Itchy"
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(
-            latitude: 33.6839,
-            longitude:  -117.8222222
-            
-        )
-        annotation.title = "Cough"
-        annotation.subtitle = "8"
-        annotations.append(annotation)
-        mapView.removeAnnotations(annotations)
-
-    }
+    
 }
