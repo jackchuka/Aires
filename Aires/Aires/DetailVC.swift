7d2d277214a9bcc9ffb71350f468e3892f0ec961
jackchuka <jack16.m.u@gmail.com>
2016-04-23 17:30:33 -0700

    new chart files

--- Aires/Aires/DetailVC.swift
+++ Aires/Aires/DetailVC.swift
@@ -7,10 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import PNChartSwift
 
 class DetailVC: UIViewController {
     
+    @IBOutlet weak var nameText: UILabel!
+    
+    let url = "http://masanoriuehara.com/api/aircheck/";
+    
+    var json = JSON([])
+    var user_id: String!
+    
     override func viewDidLoad() {
         
+        print(self.user_id)
+        
+        let params = [
+            "user_id" : self.user_id,
+            "func" : "userTimeline",
+            "limit" : 1
+        ];
+        
+        Alamofire.request(.GET, url, parameters: params as? [String : AnyObject], encoding: ParameterEncoding.URL).responseJSON { (_, _, result) in
+            switch result {
+            case .Success(let data):
+                self.json = JSON(data)
+                
+                self.displayDetails()
+                
+            case .Failure(_, let error):
+                print("Request failed with error: \(error)")
+            }
+        }
     }
+    
+    
+    func displayDetails() {
+        
+        self.nameText.text = self.json[0]["user_name"].rawString()
+        
+        self.createBarChart()
+        
+    }
+    
+    func createBarChart() {
+        
+    }
+    
 }
--- Aires/Aires/ProfileVC.swift
+++ Aires/Aires/ProfileVC.swift
@@ -16,7 +16,8 @@ class ProfileVC: UIViewController {
     
     let types = ["Cough", "Wheezing", "Sneezing", "Itchy Eyes"]
     let colors = []
-    var json = JSON([]);
+    var json = JSON([])
+    var selected: String!
     
     @IBOutlet weak var tableview: UITableView!
     
@@ -24,7 +25,6 @@ class ProfileVC: UIViewController {
         self.tableview.registerNib(UINib(nibName: "CardCell", bundle: nil), forCellReuseIdentifier: "cell")
 
         let params = [
-            "user_id" : 1,
             "func" : "timeline"
         ];
         
@@ -32,7 +32,6 @@ class ProfileVC: UIViewController {
             switch result {
             case .Success(let data):
                 self.json = JSON(data)
-                print(self.json)
                 self.tableview.reloadData()
             case .Failure(_, let error):
                 print("Request failed with error: \(error)")
@@ -71,9 +70,10 @@ class ProfileVC: UIViewController {
         return cell
         
     }
-   
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
+        selected = self.json[indexPath.row]["user_id"].rawString()
         self.performSegueWithIdentifier("detail", sender: self)
         
     }
@@ -81,6 +81,7 @@ class ProfileVC: UIViewController {
         
         if(segue.identifier == "detail"){
             let vc: DetailVC = segue.destinationViewController as! DetailVC
+            vc.user_id = selected
         }else{
             let vc: SelectionVC = segue.destinationViewController as! SelectionVC
 
@@ -93,27 +94,7 @@ class ProfileVC: UIViewController {
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 125.0
     }
-    /*
-    func parse() {
-        
-            do {
-                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
-                let jsonObj = JSON(data: data)
-                if jsonObj != JSON.null {
-                    let dict = jsonObj.dictionary!
-                    for key in dict.keys {
     
-                    }
-                    print("jsonData:\(jsonObj)")
-                } else {
-                    print("could not get json from file, make sure that file contains valid json.")
-                }
-            } catch let error as NSError {
-                print(error.localizedDescription)
-            }
-      
-    }
-*/
     @IBAction func unwindToMap(segue: UIStoryboardSegue) {
 
     }
